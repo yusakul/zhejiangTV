c984191eee7cb22d49bb6af287740f72fb3e5772
yusakul <32380039+yusakul@users.noreply.github.com>
2019-02-11 11:59:21 +0800

    Add files via upload

---
+++ chinabluetv/main.cpp
@@ -0,0 +1,82 @@
+﻿#include <stdio.h>
+
+#include <windows.h>
+#include <string>
+#include <time.h>
+
+
+#include "tool.h"
+
+using namespace md5;
+using namespace std;
+
+void getRequestUrl(char* strTime, char* strUrl)
+{
+	MD5 md5;
+
+	//源
+	const char* source[] = { "tx", "yf", "ali" };
+	//mount_name
+	char mount_name[] = "lantian";
+	//channel
+	char channel[] = "channel01";
+
+
+	//清晰度
+	const char* streamtypep[] = { "360p", "540p", "720p" };
+	//key
+	string str = "cztv/lantian/channel01";
+	char strKey[256] = {};
+	sprintf(strKey, "%s%s", str.data(), strTime);
+
+
+	//拼接url
+	string url = "http://";
+	url += source[0];
+	url += ".l.cztv.com/channels/";
+	url += mount_name;
+	url += "/";
+	url += channel;
+	url += "/";
+	url += streamtypep[2];
+	url += ".m3u8";
+	url += "?k=";
+	url += md5.digestString(strKey);//根据时间戳获取key
+	url += "&t=";
+	url += strTime;
+
+	memcpy(strUrl, url.data(), url.length());
+	return ;
+}
+
+int main()
+{
+	char strTime[20] = {};
+	char url[0x100] = {};
+
+	printf("输入时间戳（输入0则默认为当前时间）: ");
+	scanf_s("%s", strTime, 20);
+
+	if (!strcmp(strTime, "0"))
+	{
+		//时间戳
+		time_t myt = time(NULL);  //获取当前时间
+		sprintf_s(strTime, "%d", myt);
+	}
+
+	getRequestUrl(strTime, url);
+	printf("%s\n", url);
+
+	char content[1024] = {};
+	AccessWeb(url, content);
+	
+	decrypt((DWORD*)content);
+
+	system("pause");
+	return 0;
+
+	//点播
+	//http://api.cms.cztv.com/mms/out/video/playJson?id=XXXXXX&domain=www.letv.com&splatid=123&platid=1000&pt=2&at=1
+}
+
+
---
+++ chinabluetv/tool.cpp
@@ -0,0 +1,78 @@
+#include "tool.h"
+
+
+int decrypt(DWORD* pStream)
+{
+	if (pStream == NULL)
+	{
+		return 0;
+	}
+
+	int i, iByteNum, j, k;
+	FILE *pFileA;
+
+	fopen_s(&pFileA, "D:\\result.m3u8", "wb+");
+
+	
+	while( *pStream!=NULL)
+	{
+		DWORD dwTmp = *pStream++;
+		//dwTmp ^= 0x63C399ED;
+		dwTmp ^= 0xED99C363;
+
+		for (k = 0; k <= 3; k++)
+		{
+			fputc(dwTmp >> (k * 8), pFileA);
+		}
+	}
+	fclose(pFileA);
+
+	return 0;
+}
+
+//回调函数
+size_t http_data_writer(void* data, size_t size, size_t nmemb, void* content)
+{
+	long totalSize = size * nmemb;
+	std::string* symbolBuffer = (std::string*)content;
+	if (symbolBuffer)
+	{
+		symbolBuffer->append((char *)data, ((char*)data) + totalSize);
+	}
+	return totalSize;
+}
+
+
+bool AccessWeb( char* szUrl, char* content)
+{
+	CURL* curl = NULL;
+	curl = curl_easy_init();
+	CURLcode code;
+	// 设置Url
+	curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_easy_setopt(curl, CURLOPT_URL, szUrl);
+	// 设置回调函数
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, http_data_writer);
+	//设置写数据
+	std::string strData;
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&strData);
+	// 执行请求
+	code = curl_easy_perform(curl);
+
+	if (code == CURLcode::CURLE_OK)
+	{
+		long responseCode = 0;
+		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);
+		if (responseCode < 200 || responseCode >= 300 || strData.empty())
+		{
+			return false;
+		}
+		//dui应答的数据进行处理
+		memcpy(content, strData.data(), strData.length());
+	}
+
+	// 清除curl对象
+	curl_easy_cleanup(curl);
+
+	return true;
+}
